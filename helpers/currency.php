8bc08b1d84fdf4a8e6cd5925c8078b073a010bb4
neil2ball <neil2ball@gmail.com>
2025-09-23 20:21:29 +0200

    bugfix

--- helpers/currency.php
+++ helpers/currency.php
@@ -20,7 +20,7 @@
 
 // error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-require_once __DIR__ . '/includes/config.php';
+require_once __DIR__ . '/includes/config.wp.php';
 require_once __DIR__ . '/includes/economy.php';
 
 //
